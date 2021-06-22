7129212b163efc1fb4a5fb3519e7b170cf69e49f
mukirichristine <cmukiri808@gmail.com>
2021-06-22 16:55:52 +0300

    Added hot reload

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+declare const module: any;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
+
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
